f513539d870a16e984b4f6bea4a88a5ae9886611
Slv <slv.ivn@ya.ru>
2018-08-02 21:22:02 +1000

    *

---
+++ taiga/js/main.js
@@ -0,0 +1,26 @@
+
+
+$(document).ready(function(){
+
+
+
+//Подключение SVG-иконок
+
+    $('#svg_box').load('img/all_svg.svg');
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() >= 500){
+        $('.header').addClass('fixed');}
+        else { 
+            $('.header').removeClass('fixed'); }
+    });
+
+    $('[data-fancybox="mod_win"]').fancybox({
+        touch: false
+    });
+
+
+});
+
+
+

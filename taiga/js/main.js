5943d6532f62f518e04abc94baa01b8f1d0b86f0
Slv <slv.ivn@ya.ru>
2018-08-07 10:35:16 +1000

    *

--- taiga/js/main.js
+++ taiga/js/main.js
@@ -10,10 +10,37 @@ $(document).ready(function(){
 
     $(window).scroll(function(){
         if($(this).scrollTop() >= 500){
-        $('.header').addClass('fixed');}
+        $('.header').addClass('fixed');
+        $('.to_top').addClass('fixed'); }
         else { 
-            $('.header').removeClass('fixed'); }
+            $('.header').removeClass('fixed');
+            $('.to_top').removeClass('fixed'); }
     });
+
+
+    $('.to_top').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    });
+
+
+    $('.gallery').on('mobile_gallery', function(){
+        $('.gallery .g_space.scrollyeah .image_link').unwrap('.image_box');
+    });
+
+    $(window).on('load resize', function(){
+        var win = $(this);
+        if (win.width() < 842) { $('.gallery .g_space').addClass('scrollyeah'); }
+        // else { $('.gallery .g_space').removeClass('scrollyeah'); }
+        $('.gallery').trigger('mobile_gallery');
+        $('.scrollyeah').scrollyeah(); 
+    });
+
+    $('.menu .menu_icon').click(function(){
+        $(this).toggleClass('active');
+        $('.menu ul').toggleClass('visible');
+    });
+
 
     $('[data-fancybox="mod_win"]').fancybox({
         touch: false,

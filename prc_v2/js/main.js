49c1a22740aa6a479ac4fb263ce74d56f9008d27
Slv <slv.ivn@ya.ru>
2019-08-30 10:59:47 +1000

    *

---
+++ prc_v2/js/main.js
@@ -0,0 +1,27 @@
+
+
+$(document).ready(function(){
+
+
+
+//Подключение SVG-иконок
+
+    $('#svg_icons').load('img/all_icons.svg');
+
+
+
+
+
+/* Последовательная анимация
+
+    $(function() {
+      $('.').each(function(i) {
+      $(this).delay((i++) * 500).fadeTo(1000, 1); })
+    });
+*/
+
+
+});
+
+
+

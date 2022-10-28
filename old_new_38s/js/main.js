081d53d17bba8b6526ebb59ee232f91f0db59fbb
Slv <slv.ivn@ya.ru>
2022-10-28 15:28:19 +1000

    *

---
+++ old_new_38s/js/main.js
@@ -0,0 +1,56 @@
+
+$(document).ready(function(){
+
+
+	//Фансибокс-оболочка
+	$('[data-fancybox').fancybox({
+		touch: false
+	});
+
+
+	//Кнопка вернуться наверх
+	$(window).scroll(function(){
+        if($(this).scrollTop() >= 480){
+        $('.to_top').addClass('fixed'); }
+        else { 
+            $('.to_top').removeClass('fixed'); }
+    });
+	$('.to_top').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    });
+
+    //Плавающая шапка
+	$(window).scroll(function(){
+        if($(this).scrollTop() >= 480){
+        $('.header').addClass('fixed');
+        $('body').css({'paddingTop' : '216px'}); }
+        else { 
+            $('.header').removeClass('fixed');
+            $('body').css({'paddingTop' : '0'}); }
+    });
+
+
+	//Показать обрезанное содержимое блоков
+	$('.btn.show_content').click(function(){
+		$(this).hide();
+		$(this).parent().addClass('show_full');
+	});
+
+	//Показать-спрятать меню навигации
+	$('.header .sh_menu').click(function(){
+		$(this).toggleClass('active');
+		$(this).parent().find('.header_navigation').toggleClass('active');
+	});
+
+
+	/* Последовательная анимация
+
+	    $(function() {
+	      $('.').each(function(i) {
+	      $(this).delay((i++) * 500).fadeTo(1000, 1); })
+	    });
+	*/
+
+
+});

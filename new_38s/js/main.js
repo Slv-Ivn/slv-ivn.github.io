33d7159b80bd6883a695b7a7464858b8bd5fc36f
Slv-Ivn <slv.ivn@ya.ru>
2022-12-19 17:45:31 +1000

    *

--- new_38s/js/main.js
+++ new_38s/js/main.js
@@ -1,7 +1,6 @@
 
 $(document).ready(function(){
 
-
 	//Фансибокс-оболочка
 	Fancybox.bind("[data-fancybox]", {
 		autoFocus: false,
@@ -30,6 +29,38 @@ $(document).ready(function(){
             $('body').css({'paddingTop' : '0'}); }
     });
 
+    //Выбор даты в калькуляторе
+    $.fn.datepicker.setDefaults({
+        language: 'ru-RU',
+        format: 'dd.mm.YYYY',
+        days: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+        daysShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        daysMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+        months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+        monthsShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+        weekStart: 1,
+        offset: '4',
+    });
+
+    $('.form_search_aparts').ready(function(){
+        $('input[name="arrival_d"]').datepicker({
+            autoHide: true,
+            startDate: '<?=date("d.m.Y")?>',
+            trigger: $('input[name="arrival_d"]').parent()
+        }).on('pick.datepicker', function(e){
+            $('input[name="leave_d"]').datepicker('setStartDate', e.date)
+            if($('input[name="leave_d"]').datepicker('getDate') < e.date || $('input[name="leave_d"]').val().length < 5 || $('input[name="leave_d"]').val().length > 10){
+                $('input[name="leave_d"]').datepicker('setDate', e.date);
+            }
+        });
+
+        $('input[name="leave_d"]').datepicker({
+        	autoHide: true,
+        	startDate: '<?=date("d.m.Y")?>',
+        	trigger: $('input[name="leave_d"]').parent()
+        });
+
+     });
 
 	//Показать обрезанное содержимое блоков
 	$('.btn.show_content').click(function(){
@@ -43,6 +74,11 @@ $(document).ready(function(){
 		$(this).parent().find('.header_navigation').toggleClass('active');
 	});
 
+	//Показать скрытый текст в номерах
+	$('.aparts_result .ar_item .link.show_all').click(function(){
+		$(this).hide();
+		$(this).parent().next('.ari_info').show();
+	});
 
 	/* Последовательная анимация
 
